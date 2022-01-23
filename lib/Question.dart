29c071c6ce27fbee96c18b5f251c5ccc444c51c0
JiteshArul <jithesharul@gmail.com>
2022-01-24 02:21:56 +0530

    font size

--- lib/Question.dart
+++ lib/Question.dart
@@ -8,7 +8,7 @@ class Question extends StatelessWidget {
     return Text(
       questionText,
       textAlign: TextAlign.center,
-      style: TextStyle(fontSize: 35, decorationColor: Colors.blue[500]),
+      style: TextStyle(fontSize: 45, decorationColor: Colors.blue[500]),
     );
   }
 }
